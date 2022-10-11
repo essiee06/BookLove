aee5440eb8f65c63be70a99bbeed586199a7d998
aikablanes <blanesjoejeaika@gmail.com>
2022-10-11 15:39:02 +0800

    Once manage tab is clicked, can now click on other tabs

--- src/Components/Manage/Manage.js
+++ src/Components/Manage/Manage.js
@@ -1,31 +1,60 @@
 import React from "react";
 import { Button, Container, Stack } from "react-bootstrap";
-import CreateClubs from "../../Pages/CreateClub/CreateClubs";
-import EditClub from "../EditClub/EditClub";
 import styles from "./Manage.module.css";
+import { Avatar } from "@mui/material";
 
 const Manage = () => {
   return (
-    <Container className={styles.EditClubWrapper}>
-      {/* <div className={styles.EditClub}>
-        <Stack gap={4}>
-          <label className={styles.editclubtxt}>Edit Club Details</label>
-          <label className={styles.editclubdesc}>
-            Edit your Club Name, Club Icon, and Club Description here.
-          </label>
-        </Stack>
-        <Button
-          className={styles.Editbtn}
-          href="/clubpage/edit"
-          variant="danger"
-        >
-          Edit Club Details
-        </Button>
-      </div> */}
-      <div>
-        <EditClub />
-      </div>
-    </Container>
+    <form className={styles.EditClubForm}>
+        <div class="row" className={styles.EditBookClubInformation}>
+          <div class="col-md">
+            <div className={styles.EditBookClubName}>
+              <label for="ClubName">Book Club Name</label>
+              <input type="text" id="ClubName"></input>
+            </div>
+            <div>
+              <label for="ClubDesc" class="CreateClubsLabels">
+                Book Club Description
+              </label>
+              <textarea
+                type="text"
+                id="ClubDesc"
+                placeholder="Describe your Book Club briefly to attract members."
+              ></textarea>
+            </div>
+          </div>
+          <div class="col-md">
+            <div className={styles.EditWelcomeMessage}>
+              <label for="WelcomeMessage" class="CreateClubsLabels">
+                Welcome Message
+              </label>
+              <textarea
+                className={styles.EditedWelcomeMessage}
+                type="text"
+                placeholder="Enter a message that will be shown to the members when visiting the club."
+              ></textarea>
+            </div>
+            <div>
+              <Button id="CreateClub" className={styles.EditClubbuttonlabel}>
+                Save Changes
+              </Button>
+            </div>
+          </div>
+          <div class="col-md">
+            <div className={styles.EditClubPicture}>
+              <div className={styles.clubprofile}>
+                <Avatar
+                  className={styles.clubpic}
+                  //   src={url}
+                  sx={{ width: 150, height: 150 }}
+                />
+                <input className={styles.intclubcpic} id="pic" type="file" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+
   );
 };
 
