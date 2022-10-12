e36b3ea3909737ebbe3dfbe2dcff69a942258f92
Joshua Romano <joshromano13@gmail.com>
2022-10-12 12:43:35 +0800

    Welcome and createpost

--- src/Components/CreateNewPost/CreateNewPost.js
+++ src/Components/CreateNewPost/CreateNewPost.js
@@ -4,7 +4,9 @@ import { useNavigate } from "react-router-dom";
 import { auth, db } from "../firebase";
 import styles from "./CreateNewPost.module.css";
 import ClubFeed from "../ClubFeed/ClubFeed";
-import { Container } from "react-bootstrap";
+import { Container, Figure, Stack } from "react-bootstrap";
+import { Avatar } from "@mui/material";
+import * as icon from "react-icons/fa";
 
 const CreateNewPost = (Slug) => {
   // const [title, setTitle] = useState("");
@@ -15,40 +17,82 @@ const CreateNewPost = (Slug) => {
   let navigate = useNavigate();
 
   const createPost = async () => {
-  
     var today = new Date();
-    const millis = (today.getTime()).toString();
+    const millis = today.getTime().toString();
     console.log(millis);
-    const date = (today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '  ' + today.getHours() + ':' + today.getMinutes());
-    
-    await getDoc(doc(db, "Book_Club_Information", dataslug)).then((docSnap) => {
-      if (docSnap.exists()) {
-        setDoc(doc(db, "Book_Club_Information", dataslug, "Posts", millis), {
-          AuthorName: auth.currentUser.displayName, 
-          AuthorId: auth.currentUser.uid, 
-          AuthorPhoto: auth.currentUser.photoURL,
-          Post: postText,
-          BookClub_Name: docSnap.data().BookClub_Name,
-          BookClub_Picture: docSnap.data().BookClub_Picture,
-          BookClub_Slug: docSnap.data().BookClub_Slug,
-          Date_Posted: date,
-        }).then(() =>{
-        setDoc(doc(db, "Users_Information", auth.currentUser.uid, "Posts", millis), {
-          AuthorName: auth.currentUser.displayName, 
-          AuthorId: auth.currentUser.uid, 
-          AuthorPhoto: auth.currentUser.photoURL,
-          Post: postText,
-          BookClub_Name: docSnap.data().BookClub_Name,
-          BookClub_Picture: docSnap.data().BookClub_Picture,
-          BookClub_Slug: docSnap.data().BookClub_Slug,
-          Date_Posted: date,
-        })
+    const date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate() +
+      "  " +
+      today.getHours() +
+      ":" +
+      today.getMinutes();
+
+    await getDoc(doc(db, "Book_Club_Information", dataslug))
+      .then((docSnap) => {
+        if (docSnap.exists()) {
+          setDoc(doc(db, "Book_Club_Information", dataslug, "Posts", millis), {
+            AuthorName: auth.currentUser.displayName,
+            AuthorId: auth.currentUser.uid,
+            AuthorPhoto: auth.currentUser.photoURL,
+            Post: postText,
+            BookClub_Name: docSnap.data().BookClub_Name,
+            BookClub_Picture: docSnap.data().BookClub_Picture,
+            BookClub_Slug: docSnap.data().BookClub_Slug,
+            Date_Posted: date,
+          }).then(() => {
+            setDoc(
+              doc(
+                db,
+                "Users_Information",
+                auth.currentUser.uid,
+                "Posts",
+                millis
+              ),
+              {
+                AuthorName: auth.currentUser.displayName,
+                AuthorId: auth.currentUser.uid,
+                AuthorPhoto: auth.currentUser.photoURL,
+                Post: postText,
+                BookClub_Name: docSnap.data().BookClub_Name,
+                BookClub_Picture: docSnap.data().BookClub_Picture,
+                BookClub_Slug: docSnap.data().BookClub_Slug,
+                Date_Posted: date,
+              }
+            );
+          });
+        }
+      })
+      .then(() => {
+        navigate("/home");
       });
+  };
+
+  const [ProfPic, setProfPic] = useState(null);
+  auth.onAuthStateChanged((user) => {
+    var userUid = auth.currentUser.uid;
+    var docRef = doc(db, "Users_Information", userUid);
+
+    if (auth.currentUser == null) {
+      navigate("/");
     }
-    }).then(() =>{
-      navigate("/home");
-    })
-  }
+
+    if (user) {
+      getDoc(docRef)
+        .then((doc) => {
+          if (doc.exists) {
+            //DISPLAY NAME & PROFILE PICTURE
+            setProfPic(doc.data().Profile_Picture);
+          }
+        })
+        .catch((error) => {
+          console.log("Error getting document:", error);
+        });
+    }
+  });
 
   // useEffect(() => {
   //   if (!isAuth) {
@@ -57,10 +101,10 @@ const CreateNewPost = (Slug) => {
   // }, []);
 
   return (
-    <div className={styles.createPostPage}>
-      <Container fluid className={styles.WelcomeMessage}>
+    <div className={styles.welcomeContainer}>
+      <div fluid className={styles.WelcomeMessage}>
         {welcome_message}
-      </Container>
+      </div>
       <div className={styles.cpContainer}>
         {/* <h1>Share your thoughts...</h1> */}
         {/* <div className={styles.inputGp}>
@@ -71,15 +115,26 @@ const CreateNewPost = (Slug) => {
           />
         </div> */}
         <div className={styles.inputGp}>
-          {/* <label>Post:</label> */}
-          <textarea
-            className={styles.textbox}
-            placeholder="Share your thoughts..."
-            onChange={(event) => setPostText(event.target.value)}
-          />
-          <button className={styles.btnpost} onClick={createPost}>
-            Submit Post
-          </button>
+          <Stack direction="horizontal" gap={5}>
+            <Figure>
+              <Avatar
+                className={styles.profileUserImg}
+                sx={{ width: 60, height: 60 }}
+                src={ProfPic}
+              />
+              <textarea
+                className={styles.textbox}
+                placeholder="Share your thoughts..."
+                onChange={(event) => setPostText(event.target.value)}
+              />
+              {/* <button className={styles.btnpost} onClick={createPost}> */}
+              <icon.FaPencilAlt
+                className={styles.btnpost}
+                onClick={createPost}
+              />
+              {/* </button> */}
+            </Figure>
+          </Stack>
         </div>
       </div>
     </div>
